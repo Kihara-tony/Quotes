5d548c462bb62679e7826a64ecf06c4f6b7a97f8
Kihara-tony <tonykiharatonkin6@gmail.com>
2019-06-02 12:39:30 +0300

    added content in the app.module.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { HighlightthroughDirective } from './highlightthrough.directive';
 import { DateCountPipe } from './date-count.pipe';
-import { GoalFormComponent } from './goal-form/goal-form.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +16,11 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
     QuoteDetailsComponent,
     HighlightthroughDirective,
     DateCountPipe,
-    GoalFormComponent
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
