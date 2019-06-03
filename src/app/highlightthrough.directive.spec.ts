fbae94a92ee99475f65d1aaec4a0002ffb70a5d1
Kihara-tony <tonykiharatonkin6@gmail.com>
2019-06-03 08:33:29 +0300

    added a quote link.

--- src/app/highlightthrough.directive.spec.ts
+++ src/app/highlightthrough.directive.spec.ts
@@ -2,7 +2,7 @@ import { HighlightthroughDirective } from './highlightthrough.directive';
 
 describe('HighlightthroughDirective', () => {
   it('should create an instance', () => {
-    const directive = new HighlightthroughDirective();
+    const directive= new HighlightthroughDirective("");
     expect(directive).toBeTruthy();
   });
 });
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,6 @@
 export class Quote {
   public showDescription:boolean;
+  completeDate: Date;
   constructor(public id:number, public name:string, public description:string ,public timeSpan:Date){
     this.showDescription=false
   }
